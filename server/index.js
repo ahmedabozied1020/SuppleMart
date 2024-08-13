b1b0e21aac3fabba6f45625ff7d329b53d5385f1
AHmedEMadEG <amoemo141996@gmail.com>
2024-08-13 05:28:22 +0300

    client and server created

---
+++ server/index.js
@@ -0,0 +1,27 @@
+// MODULES
+const express = require('express');
+const mongoose = require('mongoose');
+
+const logger = require('./utils/logging/logger');
+
+
+const app = express();
+
+
+// ENV FILE CONFIGURATION
+require("dotenv").config();
+
+PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.DB_URL).then(() => {
+  logger.log({
+    level: "info",
+    message: "Connected to DB",
+  });
+  app.listen(process.env.PORT, () =>
+    logger.log({
+      level: "info",
+      message: `server running on port ${PORT}`,
+    })
+  );
+});
---
+++ server/utils/logging/logger.js
@@ -0,0 +1,25 @@
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    new winston.transports.File({
+      filename: "./utils/logging/log-files/error.log",
+      level: "error",
+    }),
+    new winston.transports.File({
+      filename: "./utils/logging/log-files/combined.log",
+    }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+module.exports = logger;
