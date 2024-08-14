eb118822bb90e7dfb25f96142da3c8d36648d977
AHmedEMadEG <78830356+AHmedEMadEG@users.noreply.github.com>
2024-08-14 22:52:16 +0300

    Eyada task (#2)
    Co-authored-by: eyada <mohamed.ayada.ma@gmail.com>

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainProductCardComponent } from './components/cards/product-cards/main-product-card/main-product-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainProductCardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'app';
