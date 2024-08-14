0cf7fb57a06a7eec45e5ad47857c7702ae2db855
eyada <mohamed.ayada.ma@gmail.com>
2024-08-14 21:06:07 +0300

    Eyada task

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FilledButtonComponent } from './components/buttons/filled-button/filled-button.component';
+import { SmallCardComponent } from './cards/small-card/small-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FilledButtonComponent],
+  imports: [RouterOutlet, FilledButtonComponent, SmallCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'app';
