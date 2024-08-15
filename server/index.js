d21c10df59c17d328c4c9257f367a7a943c911b0
ziad <ziad.nouh111@gmail.com>
2024-08-15 17:19:58 +0300

    add some npm packages and middlwares in the entrypoint

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 // MODULES
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+const morgan = require("morgan");
+require("express-async-errors");
 
 const logger = require("./utils/logging/logger");
 
@@ -10,13 +14,18 @@ const app = express();
 require("dotenv").config();
 
 PORT = process.env.PORT || 5000;
+DB_URL = process.env.DB_URL;
 
-mongoose.connect(process.env.DB_URL).then(() => {
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+mongoose.connect(DB_URL).then(() => {
   logger.log({
     level: "info",
     message: "Connected to DB",
   });
-  app.listen(process.env.PORT, () =>
+  app.listen(PORT, () =>
     logger.log({
       level: "info",
       message: `server running on port ${PORT}`,
