188b296adf7c38ea357c013b72453867385ac4ea
ziad <ziad.nouh111@gmail.com>
2024-08-15 17:34:09 +0300

    add review model

---
+++ server/models/review.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose");
+
+const reviewSchema = new Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      maxLength: 200,
+      required: true,
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Review = mongoose.model("Review", reviewSchema);
