9b97efedfe5e64b6232f0cbdcbe4a1f21305c0fe
eyada <mohamed.ayada.ma@gmail.com>
2024-08-17 16:22:04 +0300

    edit card small and button and 3 parts of footer

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FilledButtonComponent } from './components/buttons/filled-button/filled-button.component';
-import { SmallCardComponent } from './cards/small-card/small-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FilledButtonComponent, SmallCardComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
