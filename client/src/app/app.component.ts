bdab587fb0a96577f334ef3f25ab3e55f0779533
AHmedEMadEG <amoemo141996@gmail.com>
2024-08-19 22:14:35 +0300

    merging eyada's work

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HomeComponent } from './pages/home/home.component';
 import { HeroSectionComponent } from './layouts/hero-section/hero-section.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { SmallCardComponent } from './components/cards/product-cards/small-card/small-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, HeroSectionComponent, FooterComponent],
+  imports: [HomeComponent, HeroSectionComponent, FooterComponent, SmallCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
