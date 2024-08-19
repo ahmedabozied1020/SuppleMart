ecb5dfc54006c879af5a440bedef3296076a1ffd
AHmedEMadEG <amoemo141996@gmail.com>
2024-08-19 21:27:25 +0300

    abozaid layouts updated

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './layouts/navbar/navbar.component';
-import { ReviewRealCustomersComponent } from './layouts/testimonials/review-real-customers.component';
-import { SalesCardsComponent } from './layouts/sales-cards/sales-cards.component';
-import { TestimonialCardComponent } from './components/cards/testimonial-card/testimonial-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ReviewRealCustomersComponent, SalesCardsComponent, TestimonialCardComponent],
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../layouts/navbar/navbar.component';
+import { ReviewRealCustomersComponent } from '../../layouts/testimonials/review-real-customers.component';
+import { SalesCardsComponent } from '../../layouts/sales-cards/sales-cards.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, ReviewRealCustomersComponent, SalesCardsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
