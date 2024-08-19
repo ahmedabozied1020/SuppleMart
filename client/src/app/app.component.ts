a650286c823e90d9007299f1481eb3b3e5544ade
AHmedEMadEG <amoemo141996@gmail.com>
2024-08-19 21:39:21 +0300

    merging eyada's branch

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './pages/home/home.component';
+import { HeroSectionComponent } from './layouts/hero-section/hero-section.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, HeroSectionComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
