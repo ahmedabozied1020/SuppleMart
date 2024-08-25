4dd8f01af9bdb79a1faa6b7a0301bc0d1a6ee3db
ziad <ziad.nouh111@gmail.com>
2024-08-25 15:00:02 +0300

    add filters components selectors to the filters layout

--- client/src/app/layouts/filters/filters.component.ts
+++ client/src/app/layouts/filters/filters.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CategoryFilterComponent } from '../../components/filters/category-filter/category-filter.component';
 import { PriceFilterComponent } from '../../components/filters/price-filter/price-filter.component';
+import { BestSellerFilterComponent } from '../../components/filters/best-seller-filter/best-seller-filter.component';
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [CategoryFilterComponent, PriceFilterComponent],
+  imports: [
+    CategoryFilterComponent,
+    PriceFilterComponent,
+    BestSellerFilterComponent,
+  ],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.css',
 })
