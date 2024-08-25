cc13c2bc2c2e85e3288fe211a9339fdd892a6247
ziad <ziad.nouh111@gmail.com>
2024-08-25 23:59:24 +0300

    add paginated-product service

--- client/src/app/services/http-requests/paginated-products/paginated-products.service.ts
+++ client/src/app/services/http-requests/paginated-products/paginated-products.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Product } from '../../../interfaces/product';
+import { PaginatedProductsResponse } from '../../../interfaces/PaginatedProductsResponse';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +9,31 @@ import { Product } from '../../../interfaces/product';
 export class PaginatedProductsService {
   constructor(private httpClient: HttpClient) {}
 
-  getProducts(): Observable<Product[]> {
-    return this.httpClient.get<Product[]>(
-      'http://localhost:5000//shop/:category?'
-    );
+  getProducts(
+    category: string = 'all',
+    page: number = 1,
+    limit: number = 12,
+    minPrice?: number,
+    maxPrice?: number,
+    minRating?: number,
+    searchQuery: string = ''
+  ): Observable<PaginatedProductsResponse> {
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('limit', limit.toString())
+      .set('search', searchQuery);
+
+    if (minPrice !== undefined) {
+      params = params.set('minPrice', minPrice.toString());
+    }
+    if (maxPrice !== undefined) {
+      params = params.set('maxPrice', maxPrice.toString());
+    }
+    if (minRating !== undefined) {
+      params = params.set('minRating', minRating.toString());
+    }
+
+    const url = `http://localhost:5000/products/shop/${category}`;
+    return this.httpClient.get<PaginatedProductsResponse>(url, { params });
   }
 }
