5b0ad7ea2a88c049d4585b2e4f61528198dd2ff9
ziad <ziad.nouh111@gmail.com>
2024-08-25 15:51:30 +0300

    refactor:replace deprecated nModified

--- server/utils/updateDB/addCart.js
+++ server/utils/updateDB/addCart.js
@@ -1,10 +1,13 @@
+const User = require("../../models/user.model");
+
 async function addCartField() {
   try {
     const result = await User.updateMany(
       { cart: { $exists: false } },
       { $set: { cart: [] } }
     );
-    console.log(`${result.nModified} documents were updated`);
+    console.log(`${result.matchedCount} documents matched the criteria`);
+    console.log(`${result.modifiedCount} documents were updated`);
   } catch (error) {
     console.log(error.message);
   }
--- server/utils/updateDB/addSalesCount.js
+++ server/utils/updateDB/addSalesCount.js
@@ -1,3 +1,5 @@
+const Product = require("../../models/product.model");
+
 async function addSalesCountField() {
   try {
     const result = await Product.updateMany(
