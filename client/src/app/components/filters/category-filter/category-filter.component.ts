24ee1666ffea069a3a720c201d516bd3b99c3f1f
ziad <ziad.nouh111@gmail.com>
2024-08-26 00:17:03 +0300

    add func to category filter

--- client/src/app/components/filters/category-filter/category-filter.component.ts
+++ client/src/app/components/filters/category-filter/category-filter.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Category } from '../../../interfaces/category';
 import { CategoryRequestsService } from '../../../services/http-requests/category-requests/category-requests.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-category-filter',
@@ -12,9 +13,13 @@ import { Subscription } from 'rxjs';
 })
 export class CategoryFilterComponent {
   categories!: Category[];
+
   private subscription!: Subscription;
 
-  constructor(private categoryRequestsService: CategoryRequestsService) {}
+  constructor(
+    private categoryRequestsService: CategoryRequestsService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     setTimeout(() => {
@@ -24,7 +29,14 @@ export class CategoryFilterComponent {
           this.categories = cats;
           this.categories.splice(0, 1);
         });
-    }, 5000);
+    }, 1000);
+  }
+
+  //not working yet,until using routerLink in the app
+  navigateToCategory(category: string) {
+    this.router.navigate(['products/shop/'], {
+      queryParams: { category },
+    });
   }
 
   ngOnDestroy() {
