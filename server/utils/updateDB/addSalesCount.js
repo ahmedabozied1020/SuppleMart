fa7cf16f01f43fd489c8b5496ef7684f18dec669
ziad <ziad.nouh111@gmail.com>
2024-08-25 15:29:42 +0300

    add updateDB folder in utils for functions to update documents in DB

---
+++ server/utils/updateDB/addSalesCount.js
@@ -0,0 +1,13 @@
+async function addSalesCountField() {
+  try {
+    const result = await Product.updateMany(
+      { salesCount: { $exists: false } },
+      { $set: { salesCount: 0 } }
+    );
+    console.log(`${result.nModified} documents were updated`);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+// addSalesCountField();
