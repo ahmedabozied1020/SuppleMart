fc4a16ae11a0fac6a53b1c5af20653918dc99459
ziad <ziad.nouh111@gmail.com>
2024-08-25 12:58:51 +0300

    add price-filter component (incompleted)

--- client/src/app/layouts/filters/filters.component.ts
+++ client/src/app/layouts/filters/filters.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CategoryFilterComponent } from '../../components/filters/category-filter/category-filter.component';
+import { PriceFilterComponent } from '../../components/filters/price-filter/price-filter.component';
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [CategoryFilterComponent],
+  imports: [CategoryFilterComponent, PriceFilterComponent],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.css',
 })
