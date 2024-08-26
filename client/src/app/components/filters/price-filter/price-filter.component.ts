9c67bc76314cd56f6dccfdb1a79f817b5b86e975
ziad <ziad.nouh111@gmail.com>
2024-08-26 21:13:45 +0300

    add slider component and connected it to filters-params srvice

--- client/src/app/components/filters/price-filter/price-filter.component.ts
+++ client/src/app/components/filters/price-filter/price-filter.component.ts
@@ -1,19 +1,35 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FilterParamsService } from '../../../services/http-requests/filter-params/filter-params.service';
-
+import {
+  Options,
+  LabelType,
+  NgxSliderModule,
+} from '@angular-slider/ngx-slider';
 @Component({
   selector: 'app-price-filter',
   standalone: true,
-  imports: [FormsModule],
+  imports: [NgxSliderModule],
   templateUrl: './price-filter.component.html',
   styleUrls: ['./price-filter.component.css'],
 })
 export class PriceFilterComponent {
-  minPrice: number = 60;
-  maxPrice: number = 760;
-
+  minPrice: number = 100;
+  maxPrice: number = 400;
+  options: Options = {
+    floor: 0,
+    ceil: 500,
+    translate: (value: number, label: LabelType): string => {
+      switch (label) {
+        case LabelType.Low:
+          return '<b>Min price:</b> $' + value;
+        case LabelType.High:
+          return '<b>Max price:</b> $' + value;
+        default:
+          return '$' + value;
+      }
+    },
+  };
   constructor(
     private filtersParams: FilterParamsService,
     private router: Router
