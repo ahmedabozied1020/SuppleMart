94cabb4e761695e1064fe00be65c85b6e6825776
ziad <ziad.nouh111@gmail.com>
2024-08-26 21:17:49 +0300

    add shop page

--- client/src/app/pages/shop/shop.component.ts
+++ client/src/app/pages/shop/shop.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FiltersComponent } from '../../layouts/filters/filters.component';
 import { ShopProductsComponent } from '../../layouts/shop-products/shop-products.component';
+import { PaginationBarComponent } from '../../layouts/pagination-bar/pagination-bar.component';
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [FiltersComponent, ShopProductsComponent],
+  imports: [FiltersComponent, ShopProductsComponent, PaginationBarComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css',
 })
