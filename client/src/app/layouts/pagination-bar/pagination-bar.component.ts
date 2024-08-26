f056e148395c78f0d80c06c803c4329ebb7fdab2
ziad <ziad.nouh111@gmail.com>
2024-08-26 22:20:35 +0300

    edit pagination-bar to pass data to childern

--- client/src/app/layouts/pagination-bar/pagination-bar.component.ts
+++ client/src/app/layouts/pagination-bar/pagination-bar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { SortsFilterComponent } from '../../components/filters/sorts-filter/sorts-filter.component';
 import { ResultsSummaryComponent } from '../../components/pagination/results-summary/results-summary.component';
 import { PageNumbersComponent } from '../../components/pagination/page-numbers/page-numbers.component';
+import { Pagination } from '../../interfaces/paginationData';
 @Component({
   selector: 'app-pagination-bar',
   standalone: true,
@@ -13,4 +14,6 @@ import { PageNumbersComponent } from '../../components/pagination/page-numbers/p
   templateUrl: './pagination-bar.component.html',
   styleUrl: './pagination-bar.component.css',
 })
-export class PaginationBarComponent {}
+export class PaginationBarComponent {
+  @Input() paginationData: Pagination | undefined;
+}
