731b03b34427daacfe08bf56f7f62761db37f369
ziad <ziad.nouh111@gmail.com>
2024-08-26 21:42:01 +0300

    levitate the service to the shop page to pass data to children

--- client/src/app/layouts/shop-products/shop-products.component.ts
+++ client/src/app/layouts/shop-products/shop-products.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription, switchMap } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { Component, Input } from '@angular/core';
 import { MainProductCardComponent } from '../../components/cards/product-cards/main-product-card/main-product-card.component';
 import { OutlinedButtonComponent } from '../../components/buttons/outlined-button/outlined-button.component';
-import { PaginatedProductsService } from '../../services/http-requests/paginated-products/paginated-products.service';
-import { PaginatedProductsResponse } from '../../interfaces/PaginatedProductsResponse';
-import { FilterParamsService } from '../../services/http-requests/filter-params/filter-params.service';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-shop-products',
@@ -14,43 +10,8 @@ import { FilterParamsService } from '../../services/http-requests/filter-params/
   templateUrl: './shop-products.component.html',
   styleUrls: ['./shop-products.component.css'],
 })
-export class ShopProductsComponent implements OnInit, OnDestroy {
-  paginatedData: PaginatedProductsResponse | undefined;
-  private subscription!: Subscription;
-  private filterSubscription!: Subscription;
+export class ShopProductsComponent {
+  @Input() paginatedProducts: Product[] | undefined;
 
-  constructor(
-    private productRequestsService: PaginatedProductsService,
-    private filtersParams: FilterParamsService,
-    private route: ActivatedRoute
-  ) {}
-
-  fetchProducts() {
-    this.subscription = this.productRequestsService
-      .getProducts()
-      .subscribe((paginatedData) => {
-        this.paginatedData = paginatedData;
-      });
-  }
-
-  ngOnInit() {
-    this.route.queryParams.subscribe((params) => {
-      this.filtersParams.setFilters(params);
-    });
-
-    this.filterSubscription = this.filtersParams.filters$.subscribe(() => {
-      this.fetchProducts();
-    });
-
-    this.fetchProducts();
-  }
-
-  ngOnDestroy() {
-    if (this.filterSubscription) {
-      this.filterSubscription.unsubscribe();
-    }
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-  }
+  constructor() {}
 }
--- client/src/app/pages/shop/shop.component.ts
+++ client/src/app/pages/shop/shop.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { FiltersComponent } from '../../layouts/filters/filters.component';
 import { ShopProductsComponent } from '../../layouts/shop-products/shop-products.component';
 import { PaginationBarComponent } from '../../layouts/pagination-bar/pagination-bar.component';
+import { ActivatedRoute } from '@angular/router';
+import { FilterParamsService } from '../../services/http-requests/filter-params/filter-params.service';
+import { PaginatedProductsService } from '../../services/http-requests/paginated-products/paginated-products.service';
+import { Subscription } from 'rxjs';
+import { Product } from '../../interfaces/product';
+import { Pagination } from '../../interfaces/paginationData';
 @Component({
   selector: 'app-shop',
   standalone: true,
@@ -9,4 +15,43 @@ import { PaginationBarComponent } from '../../layouts/pagination-bar/pagination-
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css',
 })
-export class ShopComponent {}
+export class ShopComponent {
+  paginatedProducts: Product[] | undefined;
+  paginationData: Pagination | undefined;
+  private subscription!: Subscription;
+  private filterSubscription!: Subscription;
+  constructor(
+    private productRequestsService: PaginatedProductsService,
+    private filtersParams: FilterParamsService,
+    private route: ActivatedRoute
+  ) {}
+  fetchProducts() {
+    this.subscription = this.productRequestsService
+      .getProducts()
+      .subscribe((paginatedData) => {
+        this.paginatedProducts = paginatedData.paginatedProducts;
+        this.paginationData = paginatedData.pagination;
+      });
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.filtersParams.setFilters(params);
+    });
+
+    this.filterSubscription = this.filtersParams.filters$.subscribe(() => {
+      this.fetchProducts();
+    });
+
+    this.fetchProducts();
+  }
+
+  ngOnDestroy() {
+    if (this.filterSubscription) {
+      this.filterSubscription.unsubscribe();
+    }
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+}
