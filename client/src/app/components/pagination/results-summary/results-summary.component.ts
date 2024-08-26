e7e638e320e5233d87e071380e57a3623344a7cd
ziad <ziad.nouh111@gmail.com>
2024-08-26 21:19:08 +0300

    add pagination component

---
+++ client/src/app/components/pagination/results-summary/results-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-results-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './results-summary.component.html',
+  styleUrl: './results-summary.component.css'
+})
+export class ResultsSummaryComponent {
+
+}
