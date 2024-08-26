e14fb2975841df9c808528bf273015cd3bb97a9d
ziad <ziad.nouh111@gmail.com>
2024-08-26 21:17:14 +0300

    add filters-params service to shop-producrs

--- client/src/app/layouts/shop-products/shop-products.component.ts
+++ client/src/app/layouts/shop-products/shop-products.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, switchMap } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 import { MainProductCardComponent } from '../../components/cards/product-cards/main-product-card/main-product-card.component';
 import { OutlinedButtonComponent } from '../../components/buttons/outlined-button/outlined-button.component';
 import { PaginatedProductsService } from '../../services/http-requests/paginated-products/paginated-products.service';
 import { PaginatedProductsResponse } from '../../interfaces/PaginatedProductsResponse';
+import { FilterParamsService } from '../../services/http-requests/filter-params/filter-params.service';
 
 @Component({
   selector: 'app-shop-products',
@@ -15,34 +17,38 @@ import { PaginatedProductsResponse } from '../../interfaces/PaginatedProductsRes
 export class ShopProductsComponent implements OnInit, OnDestroy {
   paginatedData: PaginatedProductsResponse | undefined;
   private subscription!: Subscription;
+  private filterSubscription!: Subscription;
 
-  constructor(private productRequestsService: PaginatedProductsService) {}
+  constructor(
+    private productRequestsService: PaginatedProductsService,
+    private filtersParams: FilterParamsService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit() {
-    const category = 'all';
-    const page = 1;
-    const limit = 12;
-    const minPrice = 10;
-    const maxPrice = 100;
-    const minRating = 2;
-    const searchQuery = '';
-
+  fetchProducts() {
     this.subscription = this.productRequestsService
-      .getProducts(
-        category,
-        page,
-        limit,
-        minPrice,
-        maxPrice,
-        minRating,
-        searchQuery
-      )
+      .getProducts()
       .subscribe((paginatedData) => {
         this.paginatedData = paginatedData;
       });
   }
 
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.filtersParams.setFilters(params);
+    });
+
+    this.filterSubscription = this.filtersParams.filters$.subscribe(() => {
+      this.fetchProducts();
+    });
+
+    this.fetchProducts();
+  }
+
   ngOnDestroy() {
+    if (this.filterSubscription) {
+      this.filterSubscription.unsubscribe();
+    }
     if (this.subscription) {
       this.subscription.unsubscribe();
     }
