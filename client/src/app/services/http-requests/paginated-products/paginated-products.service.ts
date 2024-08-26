ca40379e95f5fc3065ebd9c83139368e0e050f1e
ziad <ziad.nouh111@gmail.com>
2024-08-26 16:18:06 +0300

    add shop paginated-products http service

--- client/src/app/services/http-requests/paginated-products/paginated-products.service.ts
+++ client/src/app/services/http-requests/paginated-products/paginated-products.service.ts
@@ -1,39 +1,22 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PaginatedProductsResponse } from '../../../interfaces/PaginatedProductsResponse';
+import { FilterParamsService } from '../filter-params/filter-params.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PaginatedProductsService {
-  constructor(private httpClient: HttpClient) {}
+  private baseUrl = 'http://localhost:5000/products/shop';
 
-  getProducts(
-    category: string = 'all',
-    page: number = 1,
-    limit: number = 12,
-    minPrice?: number,
-    maxPrice?: number,
-    minRating?: number,
-    searchQuery: string = ''
-  ): Observable<PaginatedProductsResponse> {
-    let params = new HttpParams()
-      .set('page', page.toString())
-      .set('limit', limit.toString())
-      .set('search', searchQuery);
+  constructor(
+    private http: HttpClient,
+    private filterParamsService: FilterParamsService
+  ) {}
 
-    if (minPrice !== undefined) {
-      params = params.set('minPrice', minPrice.toString());
-    }
-    if (maxPrice !== undefined) {
-      params = params.set('maxPrice', maxPrice.toString());
-    }
-    if (minRating !== undefined) {
-      params = params.set('minRating', minRating.toString());
-    }
-
-    const url = `http://localhost:5000/products/shop/${category}`;
-    return this.httpClient.get<PaginatedProductsResponse>(url, { params });
+  getProducts(): Observable<PaginatedProductsResponse> {
+    const params = this.filterParamsService.getHttpParams();
+    return this.http.get<PaginatedProductsResponse>(this.baseUrl, { params });
   }
 }
