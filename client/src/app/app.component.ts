32e05e88f6d72460792592ee6972db9dc4751b9c
AHmedEMadEG <amoemo141996@gmail.com>
2024-08-27 21:02:11 +0300

    added label to filled button to make it generic

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { CartComponent } from './pages/cart/cart.component';
 import { Component } from '@angular/core';
 import { HomeComponent } from './pages/home/home.component';
 import { FiltersComponent } from './layouts/filters/filters.component';
-import { CartComponent } from './pages/cart/cart.component';
 @Component({
   selector: 'app-root',
   standalone: true,
