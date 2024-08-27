a851eb84a2374f2d259f9ee2b37c7e4289bfacbb
AHmedEMadEG <amoemo141996@gmail.com>
2024-08-27 22:59:49 +0300

    added router-outlet, home, shop and cart routes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,9 @@
-import { CartComponent } from './pages/cart/cart.component';
 import { Component } from '@angular/core';
-import { HomeComponent } from './pages/home/home.component';
-import { FiltersComponent } from './layouts/filters/filters.component';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, FiltersComponent, CartComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
