bb07388dd05eddbc355d2dccf09be0b2301dfe34
ziad <ziad.nouh111@gmail.com>
2024-08-28 23:05:55 +0300

    add functionality to page-numbers component

--- client/src/app/components/pagination/page-numbers/page-numbers.component.ts
+++ client/src/app/components/pagination/page-numbers/page-numbers.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, Input, input, SimpleChanges } from '@angular/core';
+import { Pagination } from '../../../interfaces/paginationData';
+import { FilterParamsService } from '../../../services/http-requests/filter-params/filter-params.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-page-numbers',
   standalone: true,
   imports: [],
   templateUrl: './page-numbers.component.html',
-  styleUrl: './page-numbers.component.css'
+  styleUrl: './page-numbers.component.css',
 })
 export class PageNumbersComponent {
+  @Input() paginationData: Pagination | undefined;
+  pages: number[] = [];
+  currentPage: number = 1;
+  filterSubscription!: Subscription;
 
+  constructor(
+    private filterParamsService: FilterParamsService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.filterSubscription = this.filterParamsService.filters$.subscribe(
+      (params) => {
+        this.currentPage = params.page;
+      }
+    );
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['paginationData'] && this.paginationData) {
+      this.pages = Array.from(
+        { length: this.paginationData.pages },
+        (_, i) => i + 1
+      );
+    }
+  }
+
+  selectPage(page: number) {
+    this.currentPage = page;
+    this.filterParamsService.setFilters({ page });
+    this.router.navigate([], {
+      queryParams: { page },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.selectPage(this.currentPage - 1);
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.pages.length) {
+      this.selectPage(this.currentPage + 1);
+    }
+  }
 }
