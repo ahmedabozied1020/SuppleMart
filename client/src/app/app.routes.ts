5dcb725923852c5ead3d772fc07bb39aeba873c8
AHmedEMadEG <amoemo141996@gmail.com>
2024-08-31 01:02:15 +0300

    updated back-end error handling and front-end navbar routing links

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ShopComponent } from './pages/shop/shop.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
 export const routes: Routes = [
   {
@@ -25,6 +26,11 @@ export const routes: Routes = [
     path: 'register',
     component: RegisterComponent,
     title: 'Register',
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsComponent,
+    title: 'Contact Us',
   },
   {
     path: '**',
