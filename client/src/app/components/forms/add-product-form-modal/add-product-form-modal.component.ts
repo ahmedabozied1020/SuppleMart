81fd44b15483ab491f62962341b7db3a49dc4127
ziad <ziad.nouh111@gmail.com>
2024-08-30 18:57:28 +0300

    add forms folder

---
+++ client/src/app/components/forms/add-product-form-modal/add-product-form-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-add-product-form-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-product-form-modal.component.html',
+  styleUrl: './add-product-form-modal.component.css',
+})
+export class AddProductFormModalComponent {}
