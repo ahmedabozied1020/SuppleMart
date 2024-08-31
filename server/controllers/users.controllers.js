601890c810e10629ba75ac9bfeb84bfa6e96c5db
AHmedEMadEG <amoemo141996@gmail.com>
2024-09-01 00:47:26 +0300

    fixed merge bugs

--- server/controllers/users.controllers.js
+++ server/controllers/users.controllers.js
@@ -5,28 +5,26 @@ const jwt = require("jsonwebtoken");
 const jwtSign = util.promisify(jwt.sign);
 const { createUserSchema } = require("../utils/validations/users.validation");
 const CustomError = require("../utils/errors/CustomError");
-const { Admin } = require("mongodb");
+const Product = require("../models/product.model");
 
 exports.signup = async (req, res, next) => {
   try {
-    const { error } = createUserSchema.validate(req.body);
+    const { error } = await createUserSchema.validateAsync(req.body);
 
-    if(error) {
+    if (error) {
       throw new CustomError(error.details[0].message, 400);
     }
 
     const { name, email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
-if (existingUser){
-      throw new CustomError("this email is already in use", 409);
-    }
+    if (existingUser) return res.status(409).send("Email is already used.");
 
     const user = new User({ name, email, password });
     await user.save();
-    res.status(201).send({ success: "Successfully registered" });
+    res.send({ message: "User created", user });
   } catch (error) {
-    next(error);
+    throw new CustomError(error.details[0].message, 500);
   }
 };
 
@@ -135,46 +133,6 @@ exports.createAdmin = async (req, res, next) => {
     await user.save();
     res.status(201).send({ message: "Admin Created", user });
   } catch (error) {
-    throw new CustomError(error.details[0].message, 500);
-  }
+    throw new CustomError(error.details[0].message, 500);
+  }
 };
-    if (existingUser){
-      throw new CustomError("this email is already in use", 409);
-    }
-
-    const user = new User({ name, email, password });
-    await user.save();
-    res.status(201).send({ success: "Successfully registered" });
-  } catch (error) {
-    next(error);
-  }
-};
-
-exports.login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user)
-    return res.status(400).send({ error: "invalid email or password" });
-  // valid email
-  const isMatched = await bcrypt.compare(password, user.password);
-  if (isMatched) {
-    // correct email and password
-    const token = await jwtSign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "2d",
-    });
-    const { password, createdAt, updatedAt, __v, ..._user } = user._doc;
-    res.send({ success: "successfully logged in", user: { ..._user, token } });
-  } else {
-    res.status(400).send({ error: "invalid email or password" });
-  }
-};
-
-// exports.creatAdmin = async (req, res) => {
-//   const { email, password, role } = req.body;
-// };
-// const { email, password, role } = req.body;
-// const existingUser = await User.findOne({ email });
-// if (existingUser) return res.status(409).send("Email is Already Used.");
-// const user = new User({ email, password, role: "admin" });
-// await user.save();
-// res.send({ message: "Admin Created", user });
