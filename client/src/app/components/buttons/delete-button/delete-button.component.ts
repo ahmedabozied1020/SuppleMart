070de2b40bc682a5aecc9c3381e7c46dc0919f33
ziad <ziad.nouh111@gmail.com>
2024-09-01 14:43:51 +0300

    add static delete-product btn

--- client/src/app/components/buttons/delete-button/delete-button.component.ts
+++ client/src/app/components/buttons/delete-button/delete-button.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../../interfaces/product';
 
 @Component({
   selector: 'app-delete-button',
   standalone: true,
   imports: [],
   templateUrl: './delete-button.component.html',
-  styleUrl: './delete-button.component.css'
+  styleUrl: './delete-button.component.css',
 })
 export class DeleteButtonComponent {
-
+  @Input() product!: Product;
 }
