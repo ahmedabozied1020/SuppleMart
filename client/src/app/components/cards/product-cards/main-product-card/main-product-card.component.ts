0dad10166e7ea90dbf5581e9ecc5a7275f155b5a
ahmedabozied1020 <ahmedabozaied24@gmail.com>
2024-09-01 21:36:04 +0200

    add route to product details

--- client/src/app/components/cards/product-cards/main-product-card/main-product-card.component.ts
+++ client/src/app/components/cards/product-cards/main-product-card/main-product-card.component.ts
@@ -1,18 +1,19 @@
+import { RouterLink } from '@angular/router';
 import { Component, Input } from '@angular/core';
 import { Product } from '../../../../interfaces/product';
 
 @Component({
   selector: 'app-main-product-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main-product-card.component.html',
-  styleUrl: './main-product-card.component.css'
+  styleUrl: './main-product-card.component.css',
 })
 export class MainProductCardComponent {
   @Input() product!: Product;
   @Input() loading: boolean = true;
 
-  ngOnInit(){
-    this.product && this.product.categories.splice(0,1);
+  ngOnInit() {
+    this.product && this.product.categories.splice(0, 1);
   }
 }
