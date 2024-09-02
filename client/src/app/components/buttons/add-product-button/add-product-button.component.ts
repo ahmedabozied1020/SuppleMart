9f7d4d3591a067e6af619ce58652594f183ed833
ziad <ziad.nouh111@gmail.com>
2024-09-02 16:33:44 +0300

    remove the modal from add-product component

--- client/src/app/components/buttons/add-product-button/add-product-button.component.ts
+++ client/src/app/components/buttons/add-product-button/add-product-button.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { AddProductFormModalComponent } from '../../forms/add-product-form-modal/add-product-form-modal.component';
 
 @Component({
   selector: 'app-add-product-button',
   standalone: true,
-  imports: [AddProductFormModalComponent],
+  imports: [],
   templateUrl: './add-product-button.component.html',
   styleUrl: './add-product-button.component.css',
 })
