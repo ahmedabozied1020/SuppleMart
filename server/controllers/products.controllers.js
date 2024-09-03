834e99a50962e205eaae1243339c9ca6ac5e81e8
ziad <ziad.nouh111@gmail.com>
2024-09-03 19:09:30 +0300

    remove console.log

--- server/controllers/products.controllers.js
+++ server/controllers/products.controllers.js
@@ -31,7 +31,9 @@ const createProduct = async (req, res, next) => {
       throw new CustomError("One or more categories are invalid", 400);
     }
 
-    const lowerCaseCategories = categories.map((category) => category.toLowerCase().trim());
+    const lowerCaseCategories = categories.map((category) =>
+      category.toLowerCase().trim()
+    );
 
     let thumbnailUrl = null;
     let imagesUrls = [];
@@ -50,7 +52,11 @@ const createProduct = async (req, res, next) => {
     // Handle multiple images upload if provided
     if (req.files && req.files.images && req.files.images.length > 0) {
       for (const file of req.files.images) {
-        const imageKitResponse = await uploadToImageKit(file, file.originalname, "product_images");
+        const imageKitResponse = await uploadToImageKit(
+          file,
+          file.originalname,
+          "product_images"
+        );
         imagesUrls.push(imageKitResponse.url);
       }
     }
@@ -136,7 +142,9 @@ const getCategories = async (req, res, next) => {
 
 const getBestSellingProducts = async (req, res, next) => {
   try {
-    const bestSellingProducts = await Product.find().sort({ salesCount: -1 }).limit(5);
+    const bestSellingProducts = await Product.find()
+      .sort({ salesCount: -1 })
+      .limit(5);
 
     res.status(200).send(bestSellingProducts);
   } catch (error) {
@@ -146,7 +154,9 @@ const getBestSellingProducts = async (req, res, next) => {
 
 const getHomeRecommendedProducts = async (req, res, next) => {
   try {
-    const recommendedProducts = await Product.find().limit(5).select("title thumbnail price rate");
+    const recommendedProducts = await Product.find()
+      .limit(5)
+      .select("title thumbnail price rate");
 
     res.status(200).send(recommendedProducts);
   } catch (error) {
@@ -243,7 +253,6 @@ const getProductsByIds = async (req, res, next) => {
   try {
     const productsIds = req.body.productsIds;
 
-    console.log(req.body)
     const products = await Promise.all(
       productsIds.map(async (id) => await Product.findById(id))
     );
@@ -289,7 +298,11 @@ const updateProduct = async (req, res, next) => {
     // Handle multiple images upload if provided
     if (req.files && req.files.images && req.files.images.length > 0) {
       for (const file of req.files.images) {
-        const imageKitResponse = await uploadToImageKit(file, file.originalname, "product_images");
+        const imageKitResponse = await uploadToImageKit(
+          file,
+          file.originalname,
+          "product_images"
+        );
         imagesUrls.push(imageKitResponse.url);
       }
       newData.images = imagesUrls;
@@ -301,7 +314,9 @@ const updateProduct = async (req, res, next) => {
       { new: true, runValidators: true }
     );
 
-    res.status(200).send({ success: "Product Updated successfully", updateProduct });
+    res
+      .status(200)
+      .send({ success: "Product Updated successfully", updateProduct });
   } catch (error) {
     next(error);
   }
