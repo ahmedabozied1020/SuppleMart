ae0ae6dae0f9da33da6ee5d890c07d7fbe44fd4c
ziad <ziad.nouh111@gmail.com>
2024-09-03 19:11:08 +0300

    handeling error

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,14 +2,23 @@ const User = require("../models/user.model");
 
 const jwt = require("jsonwebtoken");
 const util = require("util");
+const CustomError = require("../utils/errors/CustomError");
 
 const jwtVerify = util.promisify(jwt.verify);
 
 module.exports = async (req, res, next) => {
-  const { authorization: token } = req.headers;
-  const payload = await jwtVerify(token, process.env.JWT_SECRET);
-  const user = await User.findById(payload.userId);
-  if (!user) return res.status(401).send("unAuthenticated");
-  req.user = user;
-  next();
+  try {
+    const { authorization: token } = req.headers;
+
+    const payload = await jwtVerify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(payload.userId);
+
+    if (!user) throw new CustomError("unAuthenticated", 400);
+
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
 };
